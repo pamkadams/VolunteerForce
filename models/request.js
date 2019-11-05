e125bc1d86d583b5704286e629b6393901101b0e
Pam Adams <pkadams@epotential.com>
2019-11-05 12:52:27 -0500

    data setup

---
+++ models/request.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const RequestSchema = new Schema({
+  location: String,
+  lastName: {
+    type: String,
+    required: "Name is required"
+  },
+
+  firstName: {
+    type: String,
+    required: "Name is required"
+  },
+  phone: {
+    type: String,
+    required: "Phone is required"
+  },
+
+  saline_bags: Number,
+  iv_supplies: Number,
+  tape: Number,
+  gauze: Number,
+  bandages_2x2: Number,
+  bandages_4x4: Number,
+  bandages_6x9: Number,
+  benznidazoleBottomPecan: Number,
+  aspirin: Number,
+  gluscose: Number,
+  albuterol: Number,
+  Fexinidazole: Number,
+  rifampicin: Number,
+  ASMQ: Number,
+  NECT: Number
+});
+const Requests = mongoose.model("Requests", ProductSchema);
+module.exports = Requests;
